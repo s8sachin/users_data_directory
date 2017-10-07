e258331669a18631b5bab37057c1a922d7174cee
Sachin Srinivasan <ssampathkumar@qwinix.io>
2017-10-08 00:34:12 +0530

    user model changes, config changes and users get route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
+var models  = require('../models');
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  models.User.findAll().then((users) => {
+    res.send(users);
+  })
+});
+
+router.post('/', (req, res) => {
+  // Fill
 });
 
 module.exports = router;
